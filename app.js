b48f73324a56d3ee2ebc3337431014fc74c9a7cb
Isa Forsberg <isautanede@gmail.com>
2026-05-26 15:19:32 +0200

    merge?

--- app.js
+++ app.js
@@ -1,6 +1,11 @@
-console.log('hej');// TODO: lägg till funktioner
+
 
 function greet(name) {
   console.log(`Hello, ${name}!`);
 }
 greet('Alice');
+
+function add(a, b) {
+  return a + b;
+}
+console.log(add(5, 3));
